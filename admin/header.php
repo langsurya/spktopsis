635882e70e7e012f24033994b5a806a72ca1d168
er1an9 <sun_eagle@ymail.com>
2016-11-12 08:04:09 +0700

    add

---
+++ admin/header.php
@@ -0,0 +1,17 @@
+<div class='row-fluid'>
+	<div class='span12'>
+		<div class='page-header'>
+			<h1 class='pull-left'>
+				<img src="../assets/images/logo.jpg" width="50" height="50">
+				<span>Pemilihan Guru Berprestasi</span>
+			</h1>
+			<div class='pull-right'>
+				<ul class='breadcrumb'>
+					<li><a href="#"><i class='icon-calender'></i></a></li>
+					<li class='separator'></li>
+					<li><?php echo "$hari_ini, $tanggal | $jam WIB"; ?></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
