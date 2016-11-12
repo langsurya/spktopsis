42a1b2a61ebd89b7d39b1036ac7956b3f8b8b56b
er1an9 <sun_eagle@ymail.com>
2016-11-12 07:32:01 +0700

    add guru

---
+++ admin/guru.php
@@ -0,0 +1,18 @@
+<?php 
+include_once "../admin/head.php";
+?>
+<body class='contrast-red'>
+	<?php include_once 'navbar.php'; ?>
+
+	<div id='wrapper'>
+		<div id='main-nav-bg'></div>
+		<nav class='' id="main-nav">
+			<div class='navigation'>
+				<?php 
+				include_once '../admin/sidebar.php';
+				?>
+			</div>
+		</nav>
+	</div>
+
+<?php include_once 'footer.php'; ?>
